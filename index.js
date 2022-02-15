2d6886365c5b339614e840276f62937302e81d9c
Vikash <Vikashrajendran2806@gmail.com>
2022-02-15 13:00:33 +0530

    Build: Slash commands add

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.once('ready', () => {
 	console.log('Ready!');
 });
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') 
+    {
+		await interaction.reply('Pong!');
+	} 
+    else if (commandName === 'server') 
+    {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} 
+    else if (commandName === 'user') 
+    {
+		await interaction.reply('User info.');
+	}
+    else if( commandName == 'hello' || commandName == 'Hello'){
+        await interaction.reply(`Hello! ${interaction.user.tag}`);
+    }
+});
 // Login to Discord with your client's token
 client.login(token);
